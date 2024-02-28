2baded18d24e38a6e90048db278047f63c9dab2c
Alex Lazar <alexandazar@gmail.com>
2024-02-28 15:39:57 -0800

    death

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,8 @@
 use leptos::*;
+use leptos_dom::log;
 use leptos_meta::*;
 use leptos_router::*;
+use serde::{Deserialize, Serialize};
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -27,16 +29,54 @@ pub fn App() -> impl IntoView {
     }
 }
 
-/// Renders the home page of your application.
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct AsciiDeath {
+    pub killer: String, // Also works with Vec<String>
+    pub after: bool,    // Can be anything
+}
+
+#[server]
+pub async fn kill() -> Result<AsciiDeath, ServerFnError> {
+    Ok(AsciiDeath {
+        // Needs something after it in the string
+        // Seems to die on anything € or higher, aka not in character code 32-127
+        killer: "€a".to_string(),
+        // Needs something after it in the struct
+        after: true,
+    })
+}
+
 #[component]
 fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
     let (count, set_count) = create_signal(0);
     let on_click = move |_| set_count.update(|count| *count += 1);
 
+    // Can be create_resource or create_local_resource
+    // With create_resource the page dies on hydration - the "click me" stops working
+    // With create_local_resource it dies on fetch - the "click me" still works
+    let deadly_data = create_resource(move || (), |_| async move { kill().await });
+
+    // Not technically needed - will die even if this is an empty div
     view! {
         <h1>"Welcome to Leptos!"</h1>
         <button on:click=on_click>"Click Me: " {count}</button>
+        <Suspense
+            fallback=move || view! { <p>Loading...</p> }
+        >
+            {move || deadly_data.get().map(|res| match res {
+                Ok(res) => {
+                    log!("Saw res: {res:?}");
+                    view! {
+                        <p>{format!("Got data: {res:?}")}</p>
+                    }
+                },
+                Err(err) => {
+                    view! {
+                        <p>{format!("Had error: {err:?}")}</p>
+                    }
+                },
+            })}
+        </Suspense>
     }
 }
 
